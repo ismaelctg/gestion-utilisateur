c79efabb02f2c5e7eb3afa4b858a13fb22c906f8
ismaAmm <isma2mh95@gmail.com>
2025-11-30 20:30:28 +0100

    affichae utilisateur

---
+++ projet gestion-utilisateur+installation github et git/gestion utilisateur.py
@@ -0,0 +1,27 @@
+
+utilisateurs = []
+
+def hacher_mdp(mdp):
+    return mdp[::-1]
+
+# Ajouter un utilisateur
+def ajouter_utilisateur():
+    nom = input("Nom : ")
+    email = input("Email : ")
+    mdp = input("Mot de passe : ")
+    utilisateur = {
+        "nom": nom,
+        "email": email,
+        "mdp": hacher_mdp(mdp)
+    }
+    utilisateurs.append(utilisateur)
+    print(" Utilisateur ajouté !")
+
+
+def afficher_utilisateurs():
+    if not utilisateurs:
+        print("⚠️ Aucun utilisateur enregistré.")
+    else:
+        for i, u in enumerate(utilisateurs):
+            print(f"{i+1}. Nom: {u['nom']}, Email: {u['email']}, MDP: {u['mdp']}")
+
